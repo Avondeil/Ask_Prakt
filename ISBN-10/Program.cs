37b96293781e9a607a97efad4cdc16015ae217bd
Avondeil <123551851+Avondeil@users.noreply.github.com>
2024-03-17 18:38:06 +0500

    Add files via upload

---
+++ ISBN-10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите код ISBN-10 для проверки правильности контрольной цифры (пример для проверки 5-93286-005-7): ");
+string isbn_10 = Console.ReadLine().Replace("-", "");
+int sum=0, count = 10;
+// Реализуем цикл for
+for (int i = 0; i < 10; i++)
+{
+    sum += Convert.ToInt32(isbn_10[i] == 'X' ? 10 : int.Parse(isbn_10[i].ToString())*count);
+    count--;
+}
+
+bool isValid = sum % 11 == 0;
+Console.WriteLine(isValid ? "Контрольная цифра правильная, код ISBN валидный" : "Контрольная цифра не правильная, код ISBN не валидный");
