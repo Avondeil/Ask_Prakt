7eec09b0333dd862132d63a46f390c11ec712648
Avondeil <123551851+Avondeil@users.noreply.github.com>
2024-03-21 14:18:29 +0500

    Add files via upload

--- RandomDate/Program.cs
+++ RandomDate/Program.cs
@@ -1,25 +1,17 @@
-﻿Random random = new Random();
-int daysInMonth;
-// Случайный месяц от 1 до 12
-int month = random.Next(1, 13);
-// Случайный год между 2000 и 2024
-int year = random.Next(2000, 2025);
+﻿using System.Reflection.Metadata;
 
-switch (month)
+
+
+var rnd = new MyRandom();
+var res = rnd.NextDate(DateTime.Parse("01.01.2000"), DateTime.Parse("01.01.2010")).ToString("dd/MM/yyyy");
+Console.WriteLine(res);
+
+
+class MyRandom : Random
 {
-    case 2:
-        daysInMonth = DateTime.IsLeapYear(year) ? 29 : 28;
-        break;
-    case 4:
-    case 6:
-    case 9:
-    case 11:
-        daysInMonth = 30;
-        break;
-    default:
-        daysInMonth = 31;
-        break;
+    public DateTime NextDate(DateTime min, DateTime max)
+    {
+        TimeSpan dif = max - min;
+       return min.AddDays(Next(0, dif.Days));
+    }
 }
-
-int day = random.Next(1, daysInMonth + 1);
-Console.WriteLine($"Случайная дата: {day}.{month}.{year}");
